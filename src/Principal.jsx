86780745e482d16b0b52f3f478cac6c996af4e3e
COCO <jesussanjuan23@gmail.com>
2024-03-11 21:15:20 -0500

    Estructura

---
+++ src/Principal.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Principal = () => {
+  return (
+    <div>
+      <header style={{ backgroundColor: '#333', color: '#fff', textAlign: 'center', padding: '1em' }}>
+        <h1>Mis Series de Netflix Preferidas</h1>
+      </header>
+
+      <section style={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap', padding: '20px' }}>
+        
+      </section>
+    </div>
+  );
+};
+
+export default Principal;
