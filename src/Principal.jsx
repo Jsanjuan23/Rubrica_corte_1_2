8922f6f0fa85a15995e7128acf309d20aa8d8469
COCO <jesussanjuan23@gmail.com>
2024-03-11 21:24:55 -0500

    Cuerpo_estilos

--- src/Principal.jsx
+++ src/Principal.jsx
@@ -18,27 +18,29 @@ const Principal = () => {
       category: 'Comedia',
       actors: 'Actor 2, Actriz 2',
     },
-    
+
   ];
 
   return (
-    <div>
-      <header style={{ backgroundColor: '#333', color: '#fff', textAlign: 'center', padding: '2em' }}>
-        <h1>Mis Series de Netflix Preferidas</h1>
-      </header>
+    <body style={{ fontFamily: 'Arial, sans-serif', backgroundColor: '#f5f5f5', margin: '0', padding: '0', boxSizing: 'border-box' }}>
+      <div>
+        <header style={{ backgroundColor: '#333', color: '#fff', textAlign: 'center', padding: '2em' }}>
+          <h1>Mis Series de Netflix Preferidas</h1>
+        </header>
 
-      <section style={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap', padding: '20px' }}>
-        {seriesData.map(series => (
-          <div key={series.id} style={{ width: '300px', margin: '20px', padding: '15px', backgroundColor: '#fff', borderRadius: '8px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
-            <img src={series.image} alt={`Portada ${series.name}`} style={{ width: '100%', height: '200px', objectFit: 'cover', borderRadius: '8px', marginBottom: '10px' }} />
-            <h2>{series.name}</h2>
-            <p>{series.summary}</p>
-            <p>Categoría: {series.category}</p>
-            <p>Actores Principales: {series.actors}</p>
-          </div>
-        ))}
-      </section>
-    </div>
+        <section style={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap', padding: '20px' }}>
+          {seriesData.map(series => (
+            <div key={series.id} style={{ width: '300px', margin: '20px', padding: '15px', backgroundColor: '#fff', borderRadius: '8px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
+              <img src={series.image} alt={`Portada ${series.name}`} style={{ width: '100%', height: '200px', objectFit: 'cover', borderRadius: '8px', marginBottom: '10px' }} />
+              <h2>{series.name}</h2>
+              <p>{series.summary}</p>
+              <p>Categoría: {series.category}</p>
+              <p>Actores Principales: {series.actors}</p>
+            </div>
+          ))}
+        </section>
+      </div>
+    </body>
   );
 };
 
